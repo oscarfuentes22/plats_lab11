eb71fa0569703673be137cdcd4c5d441fc7aae34
franzuetouvg <franzueto@uvg.edu.gt>
2023-10-11 07:35:19 -0600

    Write Composable Preview for MealsCategoriesScreen

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
@@ -9,10 +9,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.zezzi.eventzezziapp.navigation.AppBar
 import com.zezzi.eventzezziapp.data.networking.response.MealResponse
+import com.zezzi.eventzezziapp.navigation.Navigation
+import com.zezzi.eventzezziapp.ui.theme.EventZezziAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,15 +32,52 @@ fun MealsCategoriesScreen(
         rememberedMeals.value = mealsFromTheApi.orEmpty()
     }
 
+    BuildUi(navController, rememberedMeals.value)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BuildUi(
+    navController: NavController,
+    list: List<MealResponse>
+)
+{
     Scaffold(
         topBar = {
             AppBar(title = "Categories", navController = navController)
         }
     ) {
         LazyColumn(contentPadding = it) {
-            items(rememberedMeals.value) { meal ->
+            items(list) { meal ->
                 Text(text = meal.name)
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MealsAppPreview() {
+    EventZezziAppTheme {
+        BuildUi(rememberNavController(), listOf(
+            MealResponse(
+                "id",
+                "nombre",
+                "description",
+                "url"
+            ),
+            MealResponse(
+                "id",
+                "nombre",
+                "description",
+                "url"
+            ),
+            MealResponse(
+                "id",
+                "nombre",
+                "description",
+                "url"
+            )
+        ))
     }
 }
