0249d52bac46034d6e465c3bdd3f7a60c353c349
franzuetouvg <franzueto@uvg.edu.gt>
2023-10-11 09:12:43 -0600

    Clean up

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsScreen.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsScreen.kt
@@ -1,29 +1,13 @@
 package com.zezzi.eventzezziapp.ui.meals.view
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
 import com.zezzi.eventzezziapp.navigation.AppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -33,8 +17,6 @@ fun MealsScreen(
     category: String,
     viewModel: MealsCategoriesViewModel = viewModel()
 ) {
-    viewModel.getMeals()
-
     Scaffold(
         topBar = {
             AppBar(title = "Meals for $category", navController = navController)
